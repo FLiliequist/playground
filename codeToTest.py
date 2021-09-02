8cf7e6016bde1abd78168b1809f22c0dd78e1fca
Fredrik Liliequist <fredrik@MacBook.local>
2021-09-02 14:11:21 +0200

    Added first file

---
+++ codeToTest.py
@@ -0,0 +1,12 @@
+y = 1
+import x
+logging.debug('hej')
+print(y)
+password = 'Admin'
+
+password = ''
+y == y
+y = 5
+user_inpt = input('Please enter all your secrets: ')
+x = f"Select {user_inpt} from users;"
+print(y)
